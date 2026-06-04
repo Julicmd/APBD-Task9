a564944274c4915dd9905ba1d28cc8576494fdb4
Julius Babelis <s31850@pjwst.edu.pl>
2026-06-05 00:12:21 +0200

    Dashboard and Notes

--- Controllers/DashBoardController.cs
+++ Controllers/DashBoardController.cs
@@ -1,4 +1,6 @@
+using System.Security.Claims;
 using Application_Auth.Data;
+using Application_Auth.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,8 +18,38 @@ public class DashBoardController :Controller
     [HttpGet]
     public IActionResult Index()
     {
+        var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+        var notes = _dbContext.UserNotes.Where(un =>un.AppUserId == userId).ToList();
+        
         return View();
     }
+    
+    [HttpGet]
+    public IActionResult AddNotes()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult AddNotes(UserNote model)
+    {
+        var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+
+
+        var note = new UserNote
+        {
+            AppUserId = userId,
+            Title = model.Title,
+            Content = model.Content,
+            CreatedAt = DateTime.UtcNow
+        };
+        _dbContext.UserNotes.Add(note);
+        _dbContext.SaveChanges();
+        
+        
+       return RedirectToAction("Index"); 
+    }
+    
     
 
 }
